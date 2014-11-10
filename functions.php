669d04694c3b2418d49828517cac0a966aa28a92
Chris Wilcoxson <chrisw@dccmarketing.com>
2014-11-10 16:47:31 -0600

    Event hover animation done.

--- functions.php
+++ functions.php
@@ -104,6 +104,8 @@ function logancounty_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	wp_enqueue_style( 'logancounty-animate', get_template_directory_uri() . '/sass/animate.css' );
 }
 add_action( 'wp_enqueue_scripts', 'logancounty_scripts' );
 
